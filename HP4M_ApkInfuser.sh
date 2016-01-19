0d1e6e8052764c5db7f79ed1e6680b6a3155e8c6
richard <chenecust@aliyun.com>
2016-01-19 17:42:32 +0800

    update

--- HP4M_ApkInfuser.sh
+++ HP4M_ApkInfuser.sh
@@ -1,3 +1,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo =======================
+echo $DIR
+echo =======================
 DIR=/home/travis/build/mcfortravis/nodeTest/hpmc/AndroidTools
 $DIR/ApkInfuser/ApkInfuser.sh ${1+"$@"}
